2053262fe1b66293ee5e1d66fed55bf267db9f4e
João Pedro de Araújo de Castro <234853299@seap.rj>
2026-04-20 16:55:46 -0300

    Tentei Dbeaver

---
+++ Aprendizado/Aprendendo_API/prisma/migrations/20260420195110_rename_tables/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - You are about to drop the `tasks` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "tasks" DROP CONSTRAINT "tasks_userId_fkey";
+
+-- DropTable
+DROP TABLE "tasks";
+
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- Aprendizado/Aprendendo_API/src/database/prisma.ts
+++ Aprendizado/Aprendendo_API/src/database/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaMariaDb } from "@prisma/adapter-mariadb"
-import "dotenv/config"
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL!
+const connectionString = process.env.DATABASE_URL!;
 
-const adapter = new PrismaMariaDb(connectionString)
+const adapter = new PrismaPg({ connectionString });
 
 export const prisma = new PrismaClient({
-  adapter
-})
+  adapter,
+});
